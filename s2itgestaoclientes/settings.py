3dd7349fc308e41963a057035e4af6283c342df7
Moraes <slsmoraes@gmail.com>
2021-01-15 18:35:00 -0300

    D210115H1833-Commit final

--- s2itgestaoclientes/settings.py
+++ s2itgestaoclientes/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['s2itgestaoclientes.herokuapp.com']
+ALLOWED_HOSTS = ['s2itgestaoclientes.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrapform',
     'clientes',
     'home',
 ]
@@ -129,3 +130,7 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = 'person_list'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    'statics',
+]
